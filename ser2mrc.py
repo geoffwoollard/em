7d239fe1aa17fc6931c161fefbef9eb90bdf48b4
Geoffrey Woollard <geoffwoollard@gmail.com>
2020-09-08 12:19:39 -0400

    extension option for tif and mrc. mrc default

--- ser2mrc.py
+++ ser2mrc.py
@@ -3,6 +3,10 @@ import subprocess, os, argparse
 parser = argparse.ArgumentParser(description='rename file from basefilename_1.ser to basefilename.ser')
 parser.add_argument('--filename', type=str, nargs=1,
                     help='file containing list of .ser file names')
+parser.add_argument('--ext', type=str, nargs=1,
+                    default='mrc',
+                    choices=['mrc','tif'],
+                    help='output extension')
 parser.add_argument('--info', type=str, nargs=1,
                     help='''
                     1. Make list of files to convert (FILE.txt)
@@ -11,6 +15,7 @@ parser.add_argument('--info', type=str, nargs=1,
 args = parser.parse_args()
 
 filename = args.filename[0]
+ext = args.ext[0]
 
 with open(filename) as f:
     files = f.read().split('\n')
@@ -18,8 +23,9 @@ with open(filename) as f:
 for file in files:
     if os.path.exists(file): 
         if file.endswith('.ser'):
-            command = ['e2proc2d.py',file,file.replace('.ser','.mrc')]
-            subprocess.call(command)
+            command = ['e2proc2d.py',file,file.replace('.ser','.'+ext)]
+            #subprocess.call(command)
+            print(' '.join(command))
             
         else: print('File:"%s" does not end with .ser' % file)
     else: print('File:"%s" does not exist' % file)
